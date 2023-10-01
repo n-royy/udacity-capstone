02308026bf2dea22c5f04f65346a750806ccf9c0
n-royy <roynguyen18496@gmail.com>
2023-10-02 00:44:49 +0700

    set permission for Docker

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -7,5 +7,5 @@ then
     TAG="latest"
 fi
 
-echo $DOCKER_PASSWORD | docker login -u "namnn10" --password-stdin  docker.io
+echo $DOCKER_PASSWORD | docker login -u "namnn10" --password-stdin "dckr_pat_vBM5mNHA4SE5rS5R8E9GJnlLvhs"
 docker push n-royy/udacity-capstone:$TAG
